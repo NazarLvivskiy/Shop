0ad4bf8eaa95a8c242994de02c039c3bca1b6af4
Nazar <nazarfesenko6@gmail.com>
2021-01-16 20:54:38 +0200

    Добавьте файлы проекта.

---
+++ Models/Repository/EFRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Models.Repository
+{
+    public class EFRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        public void Create(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<TEntity> GetAllEntities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<TEntity> GetEntitiesForFilter(Predicate<TEntity> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetForId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Updata(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Models/Repository/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Models.Repository
+{
+    interface IRepository<TEntity> where TEntity : class
+    {
+        //POST
+        void Create(TEntity entity);
+
+        //GET
+        ICollection<TEntity> GetAllEntities();
+
+        //GET
+        ICollection<TEntity> GetEntitiesForFilter(Predicate<TEntity> predicate);
+
+        //GET
+        TEntity GetForId(int id);
+
+        //PUT
+        void Updata(TEntity entity);
+
+        //DELETE
+        void Delete(int id);
+    }
+}
